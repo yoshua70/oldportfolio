4d4bd7a984e8adbeed9ff9c6cac5440f6023ef49
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-02-01 11:50:50 +0000

    Add Hero section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import Hero from "../components/Hero"
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl font-bold underline">Hello, World !</h1>
+      <Hero />
     </div>
   )
 }
