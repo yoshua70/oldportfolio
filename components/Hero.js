840c53f54de046dc300b239d9524c7eb0edbd7f9
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-02-01 11:55:12 +0000

    Remove padding from container

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="flex flex-col min-h-screen p-20 items-start justify-center gap-4">
+    <div className="flex flex-col min-h-screen items-start justify-center gap-4">
       <h1 className="text-5xl text-gray-400 font-thin leading-normal">
         I'm a simple developer, <br />
         making his way through the web.
