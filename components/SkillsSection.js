de6156d240bb0d8f9a9c1244a5d112b56eb46782
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-02-02 20:44:57 +0000

    Remove content

--- components/SkillsSection.js
+++ components/SkillsSection.js
@@ -3,38 +3,7 @@ export default function SkillsSection() {
     <div className="flex justify-between mt-10 mb-10">
       <div className="flex flex-col gap-5">
         <p className="text-emerald-500 font-bold">Stuff I know</p>
-        <div className="flex justify-between">
-          <div className="flex flex-col">
-            <h1 className="uppercase font-bold">Languages</h1>
-            <ul className="ml-5 list-disc">
-              <li>C</li>
-              <li>Python</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Javascript</li>
-            </ul>
-          </div>
-          <div className="flex flex-col">
-            <h1 className="uppercase font-bold">Languages</h1>
-            <ul className="ml-5 list-disc">
-              <li>C</li>
-              <li>Python</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Javascript</li>
-            </ul>
-          </div>
-          <div className="flex flex-col">
-            <h1 className="uppercase font-bold">Languages</h1>
-            <ul className="ml-5 list-disc">
-              <li>C</li>
-              <li>Python</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Javascript</li>
-            </ul>
-          </div>
-        </div>
+        <div className="flex justify-between"></div>
       </div>
     </div>
   )
