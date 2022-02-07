35aac259a2cbb9cd4120601b969081727dcbd04c
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-02-07 05:58:48 +0000

    Create navbar component. Add github link and icon in navbar.

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-end">
+      <a href="https://github.com/yoshua70" target="_blank">
+        <img src="https://img.icons8.com/material-outlined/24/000000/github.png" />
+        source
+      </a>
+    </div>
+  )
+}
