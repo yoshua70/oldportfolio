3773afe3553a3f711c1b04dfca7cd4c3f41a2def
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-02-13 21:28:21 +0000

    Change heading text. Change cursor style on hover

--- components/Hero.js
+++ components/Hero.js
@@ -2,12 +2,12 @@ export default function Hero() {
   return (
     <div className="flex flex-col min-h-screen items-start justify-center gap-4">
       <h1 className="text-5xl text-gray-400 font-thin leading-normal">
-        I'm a simple developer, <br />
+        I'm an engineer, <br />
         making his way through the web.
       </h1>
       <p>
         This is the way &nbsp;
-        <span className="underline decoration-emerald-400 decoration-2 transition ease-in-out delay-75 duration-300 hover:bg-emerald-400">
+        <span className="underline decoration-emerald-400 decoration-2 transition ease-in-out delay-75 duration-300 hover:bg-emerald-400 hover:cursor-pointer">
           allidjinoumichee@outlook.fr
         </span>
       </p>
