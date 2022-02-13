12a3800122c8df67b86250204295dfa57f1989ff
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-02-13 21:28:34 +0000

    Change links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,19 @@
+import Link from "next/link"
+
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-end">
-      <a href="https://github.com/yoshua70" target="_blank">
-        <img src="https://img.icons8.com/material-outlined/24/000000/github.png" />
-        source
-      </a>
+    <div className="flex items-center justify-end gap-4">
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/projects">
+        <a>Projects</a>
+      </Link>
+      <Link href="https://github.com/yoshua70" target="_blank">
+        <a>
+          <img src="https://img.icons8.com/material-outlined/24/000000/github.png" />
+        </a>
+      </Link>
     </div>
   )
 }
