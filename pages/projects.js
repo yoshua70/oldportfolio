7415e6da256c22ef9fcf34bf31175a2d92fc39e5
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-02-13 21:35:00 +0000

    Create projects page

---
+++ pages/projects.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+import Layout from "../components/Layout"
+import Navbar from "../components/Navbar"
+
+export default function Projects() {
+  return (
+    <Layout>
+      <Head>
+        <title>Projects - Michee Allidjinou</title>
+        <meta
+          name="description"
+          content="Personal website of Michee Allidjinou"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+    </Layout>
+  )
+}
